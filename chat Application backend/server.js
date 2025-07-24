2b8f074ad6ecb0899725133313da36e66dc3d77d
RohitBCA456 <ry2281104@gmail.com>
2025-07-24 18:55:22 +0530

    fix

--- chat Application backend/server.js
+++ chat Application backend/server.js
@@ -1,13 +1,19 @@
-import { app } from "./app.js";
+import { app, server } from "./app.js";
 import { connectDB } from "./database/db.js";
-import { server } from "./app.js";
-connectDB().then(() => {
-  app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
-  });
-});
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const PORT = process.env.PORT || 3000; // use PORT for both HTTP and Socket
+
+connectDB()
+  .then(() => {
+    app.on("error", (err) => {
+      console.error(`Server error: ${err.message}`);
+    });
+
+    server.listen(PORT, () => {
+      console.log(`Server & Socket.IO running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error(`Failed to connect to the database: ${error.message}`);
+    process.exit(1);
+  });
